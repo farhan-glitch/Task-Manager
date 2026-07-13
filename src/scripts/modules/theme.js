30d2d0613f6fd5edf4a39069c8a2bf96f366f272
Farhan <farhanesmd3@gmail.com>
2026-07-13 12:10:28 +0330

    Add local storage for theme

--- src/scripts/modules/theme.js
+++ src/scripts/modules/theme.js
@@ -1,13 +1,3 @@
-// if (
-//   localStorage.theme === "dark" ||
-//   (!(theme in localStorage) &&
-//     window.matchMedia("prefers-color-scheme: dark").matches)
-// ) {
-//   document.documentElement.classList.add("dark");
-// } else {
-//   document.documentElement.classList.remove("dark");
-// }
-
 export const themeToggle = () => {
   // DOM Selection
   const lightBtn = document.getElementById("light-btn");
@@ -15,11 +5,11 @@ export const themeToggle = () => {
   // Define Functions
   function lightTheme() {
     document.documentElement.classList.remove("dark");
-    // localStorage.theme = "light";
+    localStorage.theme = "light";
   }
   function darkTheme() {
     document.documentElement.classList.add("dark");
-    // localStorage.theme = "dark";
+    localStorage.theme = "dark";
   }
   // Event Listeners
   lightBtn.addEventListener("click", lightTheme);
