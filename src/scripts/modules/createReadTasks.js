f3654086e95f92efb70fc186e2da124efa97a829
Farhan <farhanesmd3@gmail.com>
2026-07-15 11:35:24 +0330

    Modify theme buttons position

--- src/scripts/modules/createReadTasks.js
+++ src/scripts/modules/createReadTasks.js
@@ -46,10 +46,10 @@ export const initCreateReadTasks = () => {
   });
 
   tags.addEventListener("click", () => {
-    if (img.src.includes("src/images/tag-right-1.svg")) {
-      img.src = "./src/images/tag-right-2.svg";
+    if (img.src.includes("src/assets/icons/tag-right-1.svg")) {
+      img.src = "./src/assets/icons/tag-right-2.svg";
     } else {
-      img.src = "./src/images/tag-right-1.svg";
+      img.src = "./src/assets/icons/tag-right-1.svg";
     }
 
     buttons.classList.toggle("hidden");
@@ -146,7 +146,7 @@ export const initCreateReadTasks = () => {
     </p>
   </div>
   <button class="absolute top-3 left-3">
-    <img class="w-[4px] h-[18px]" src="./src/images/option.svg" alt="Edit-Task" />
+    <img class="w-[4px] h-[18px]" src="./src/assets/icons/option.svg" alt="Edit-Task" />
   </button>
   <div class="w-[4px] h-[75%] border-[1px] ${colorBorder.border} rounded-t-[8px] rounded-b-[8px] ${colorBorder.bg} absolute right-[-2px]"></div>
 `;
@@ -285,7 +285,7 @@ export const initCreateReadTasks = () => {
     </p>
   </div>
   <button class="absolute top-3 left-3">
-    <img class="w-[4px] h-[18px]" src="./src/images/option.svg" alt="Edit-Task" />
+    <img class="w-[4px] h-[18px]" src="./src/assets/icons/option.svg" alt="Edit-Task" />
   </button>
   <div class="w-[4px] h-[75%] border-[1px] ${task.colorBorder.border} rounded-t-[8px] rounded-b-[8px] ${task.colorBorder.bg} absolute right-[-2px]"></div>
 `;
